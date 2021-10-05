d921262e2b5b71b383639a6c8dbcb9f686b70b2d
Ennio Pirolo Ryzen <sant.3nnio@gmail.com>
2021-10-05 16:49:49 +0200

    Ultima parte del corso con connessione web

--- Assets/Scenes/Scripts/InstantiatePosts.cs
+++ Assets/Scenes/Scripts/InstantiatePosts.cs
@@ -1,38 +1,120 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class InstantiatePosts : MonoBehaviour
 {
 
-    public int PostsNumber=1;
+    public string Path;
+
     public GameObject PostPrefab;
     public Transform PostParent;
 
-    public List<Texture2D> SampleTextures;
+    public MData data;
+    
     void Start()
     {
-        this.instantiatePosts();
+        //string json = File.ReadAllText(this.Path);
+        //this.data = JsonUtility.FromJson<MData>(json);
+        //this.instantiatePosts();
+
+        StartCoroutine(this.DownloadJson(Path, onProgress, onComplete));
+
     }
 
     void instantiatePosts(){
 
-        for(int i=0; i<PostsNumber; i++){
+        
+        foreach(MPost PostData in this.data.Posts){
 
-            GameObject post=GameObject.Instantiate(PostPrefab, PostParent);
-
-            Texture2D randText = this.SampleTextures[Random.Range(0, SampleTextures.Count)];
-            Texture2D randProfile = this.SampleTextures[Random.Range(0, SampleTextures.Count)];
+            GameObject PostGameObject=GameObject.Instantiate(PostPrefab, PostParent);
             
-            post.transform.Find("ImageWrapper/RawImage").GetComponent<Image>().sprite = Sprite.Create(randText, new Rect(0.0f, 0.0f, randText.width, randText.height), new Vector2(0.5f, 0.5f), 100.0f);
-            post.transform.Find("PostHeader/ImageMask/Profile").GetComponent<Image>().sprite = Sprite.Create(randProfile, new Rect(0.0f, 0.0f, randProfile.width, randProfile.height), new Vector2(0.5f, 0.5f), 100.0f);
+            //Username
+            PostGameObject.transform.Find("PostHeader/Username").GetComponent<TextMeshProUGUI>().text=PostData.Username;
 
-            post.transform.Find("PostHeader/Username").GetComponent<TextMeshProUGUI>().text="CiaoCiaoBauBau";
+            StartCoroutine(this.DownloadTexture(PostData.ImageURL,
+                (float p)=>{Debug.Log("Downloading Texture "+p);},
+                (Texture2D result, string error)=>{
+                    if(result!=null){
+                        PostGameObject.transform.Find("ImageWrapper/RawImage").GetComponent<Image>().sprite = Sprite.Create(result, new Rect(0.0f, 0.0f, result.width, result.height), new Vector2(0.5f, 0.5f), 100.0f);
+                    }
+                }
+
+            ));
 
         }
 
     }
-    
+    void onProgress(float p){
+        Debug.Log(p);
+    }
+    void onComplete( MData d, string error )
+    {
+        if(d!=null){
+            this.data=d;
+            this.instantiatePosts();
+        }
+
+    }
+    private IEnumerator DownloadJson(string url, Action<float> onProgress, Action<MData,string> onComplete)
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get(url))
+        {
+            var operation= www.SendWebRequest();
+            
+            while(!operation.isDone)
+            {	
+                onProgress( www.downloadProgress );
+                yield return null;
+            }
+            
+            if (string.IsNullOrEmpty(www.error))
+            {
+
+                var json = www.downloadHandler.text;
+                var content = JsonUtility.FromJson<MData>(json);
+
+                if (onComplete != null)
+                    onComplete(content,"");
+            }
+            else
+            {
+                Debug.LogError("Error " + www.error);
+                if (onComplete != null)
+                    onComplete(null, www.error);
+            }	
+        }
+    }
+
+    private IEnumerator DownloadTexture(string url, Action<float> onProgress, Action<Texture2D,string> onComplete)
+    {
+        using (UnityWebRequest www = UnityWebRequestTexture.GetTexture(url))
+        {
+            var operation= www.SendWebRequest();
+            
+            while(!operation.isDone)
+            {	
+                onProgress( www.downloadProgress );
+                yield return null;
+            }
+            
+            if (string.IsNullOrEmpty(www.error))
+            {
+                
+                Texture2D text = DownloadHandlerTexture.GetContent(www);
+                if (onComplete != null)
+                    onComplete(text,"");
+            }
+            else
+            {
+                if (onComplete != null)
+                    onComplete(null, www.error);
+            }	
+        }
+    }
 }
---
+++ Assets/Scenes/Scripts/MPost.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class MPost 
+{
+    public string ProfilePicURL;
+    public string Username;
+
+    public string ImageURL;
+    public string PostText;
+    
+}
